d919ff41a196fa074fe96ee60690aed0b401aec1
Travis Johnson <travis@thisguy.codes>
2023-10-30 00:51:54 -0400

    chore(docs): actually have some documentation (#6)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# modelstack
-Cleanly nested Model objects for https://github.com/charmbracelet/bubbletea
+# Modelstack
+Cleanly nested Model objects for [Bubbletea](https://github.com/charmbracelet/bubbletea)
+
+[![Go Reference](https://pkg.go.dev/badge/github.com/thisguycodes/modelstack/.svg)](https://pkg.go.dev/github.com/thisguycodes/modelstack/)
--- modelstack.go
+++ modelstack.go
@@ -6,12 +6,17 @@ import (
 	"github.com/thisguycodes/modelstack/internal/stack"
 )
 
+// PushModel is a tea.Msg that instructs a ModelStack to add a new tea.Model to
+// the stack and pass control to it
 type PushModel struct {
 	Model tea.Model
 }
 
+// PopModel is a tea.Msg that instructs a ModelStack to remove the current
+// tea.Model from the stack and pass control to the previous tea.Model
 type PopModel struct{}
 
+// Push is a tea.Cmd used to send a PushModel tea.Msg
 func Push(m tea.Model) func() tea.Msg {
 	return func() tea.Msg {
 		return PushModel{
@@ -20,10 +25,12 @@ func Push(m tea.Model) func() tea.Msg {
 	}
 }
 
+// Pop is a tea.Cmd used to send a PopModel tea.Msg
 func Pop() tea.Msg {
 	return PopModel{}
 }
 
+// New creates a new Modelstack with an initial tea.Model
 func New(m tea.Model) ModelStack {
 	return ModelStack{
 		current: m,
@@ -31,12 +38,16 @@ func New(m tea.Model) ModelStack {
 	}
 }
 
+// ModelStack is a tea.Model that listens for PushModel and PopModel tea.Msg's
+// to switch control between several tea.Model's
 type ModelStack struct {
 	current    tea.Model
 	lastResize tea.WindowSizeMsg
 	stack      *stack.Stack[tea.Model]
 }
 
+// View fulfills the tea.Model interface, rendering the currently active
+// tea.Model on its stack
 func (m ModelStack) View() string {
 	return m.current.View()
 }
@@ -47,6 +58,9 @@ func (m *ModelStack) updateCurrent(msg tea.Msg) tea.Cmd {
 	return cmd
 }
 
+// Update fulfills the tea.Model interface, intercepting PushModel and PopModel
+// tea.Msg's, as well as information needed to initialize models as they switch
+// control (e.g. the most recent tea.WindowSizeMsg)
 func (m ModelStack) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
@@ -69,6 +83,8 @@ func (m ModelStack) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
+// Init fulfills the tea.Model interface, currently it just calls Init() on the
+// current tea.Model on the stack
 func (m ModelStack) Init() tea.Cmd {
 	return m.current.Init()
 }
